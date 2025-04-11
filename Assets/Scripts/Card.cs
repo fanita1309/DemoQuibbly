b773e5f7a166fa44ef8b7921a606c0007201d06a
fanita1309 <fani04535@gmail.com>
2025-04-10 19:18:18 -0600

    cartas script

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,12 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : ScriptableObject
+namespace productions
 {
-    
-    void Start()
-    {
-        
-    }
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
 
+    public class Card : ScriptableObject
+    {
+        public string cardName;
+        public CardType cardtype;
+        public int health;
+        public int damageMin;
+        public int damageMax;
+        public DamageType damageType;
+
+        public enum CardType //tipos de cartas
+        {
+            Fire,
+
+            Earth,
+
+            Water,
+
+            Air,
+
+        }
+
+        public enum DamageType //tipos de daño
+        {
+            Fire,
+
+            Earth,
+
+            Water,
+
+            Air,
+
+        }
+
+
+    }
 }
